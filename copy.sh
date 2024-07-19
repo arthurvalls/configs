9d6532677e0d35f8f50c67b8776e521acba00b02
Arthur Valls <arthur.valls@mettainnovations.com.br>
2024-07-19 14:40:37 -0300

    utils

---
+++ copy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Copy Neovim configuration files
+cp ~/.config/nvim/* /home/arthur/configs/nvim/
+
+# Copy Alacritty configuration files
+cp ~/.config/alacritty/* /home/arthur/configs/alacritty/
+
+# Copy tmux configuration file to Alacritty directory
+cp ~/.tmux.conf /home/arthur/configs/alacritty/.tmux.conf
+
+# Copy Zsh configuration file
+cp ~/.config/ezsh/ezshrc.zsh /home/arthur/configs/zsh/
+
+echo "Configuration files have been copied successfully."
