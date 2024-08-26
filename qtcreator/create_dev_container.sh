030ed5c83fc76fab7b9ecb0ec21de74c70c84560
Arthur Valls <arthur.valls@mettainnovations.com.br>
2024-08-26 16:47:27 -0300

    gruber

--- qtcreator/create_dev_container.sh
+++ qtcreator/create_dev_container.sh
@@ -2,14 +2,14 @@
 
 # Diretorio de trabalho que deseja-se mapear para dentro do container.
 GIT_REP=/home/arthur/workspace
-IMAGE_NAME=mettainnovations/dev:latest
-CONTAINER_NAME=dev
+IMAGE_NAME=mettainnovations/dev:2.2
+CONTAINER_NAME=dev_tc
 
 docker create -it --privileged \
 	--runtime=nvidia --gpus all \
 	--name $CONTAINER_NAME \
 	--ipc=host \
-	-p 9015:9015 \
+	-p 9016:9016 \
 	--restart always \
 	-v $GIT_REP/html:/html \
 	-v $GIT_REP:/home/developer/workspace \
