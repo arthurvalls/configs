a6379a216cc088a03cffca22700246e5efedcbc7
arthurvalls <arthur.valls5@gmail.com>
2025-04-22 14:34:16 -0300

    adds: rose-pine

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -3,7 +3,7 @@ return {
   require 'config.plugins.dashboard',
   require 'config.plugins.autocomplete',
   require 'config.plugins.autoformat',
-  require 'config.plugins.cyberdream',
+  require 'config.plugins.rose-pine',
   require 'config.plugins.dap',
   require 'config.plugins.devicons',
   require 'config.plugins.gitsigns',
