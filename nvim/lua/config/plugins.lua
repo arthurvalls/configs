7f3070ea0aee62ceef0272db22cd6fffb28ae4d9
arthurvalls <arthur.valls5@gmail.com>
2025-05-01 21:21:54 -0300

    add notifications

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -1,5 +1,6 @@
 -- lua/config/plugins.lua
 return {
+  require 'config.plugins.notify',
   require 'config.plugins.everforest',
   require 'config.plugins.copilot',
   require 'config.plugins.leetcode',
