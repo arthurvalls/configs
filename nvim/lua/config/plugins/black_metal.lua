cc5b82475cdd843ee2c76ba83be6b633f95a9cc8
arthurvalls <arthur.valls5@gmail.com>
2025-05-11 19:29:30 -0300

    adds another black metal theme

--- nvim/lua/config/plugins/black_metal.lua
+++ nvim/lua/config/plugins/black_metal.lua
@@ -7,7 +7,7 @@ return {
       -----MAIN OPTIONS-----
       --
       -- Can be one of: bathory | burzum | dark-funeral | darkthrone | emperor | gorgoroth | immortal | impaled-nazarene | khold | marduk | mayhem | nile | taake | thyrfing | venom | windir
-      theme = 'bathory',
+      theme = 'marduk',
       -- Can be one of: 'light' | 'dark', or set via vim.o.background
       variant = 'dark',
       -- Use an alternate, lighter bg
