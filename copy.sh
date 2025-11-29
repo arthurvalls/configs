0230aea06e7c398cc9b80539497aee179da31b9d
arthurvalls <arthur.valls@supersim.com.br>
2025-11-29 15:19:47 -0300

    fixing vim backwards

--- copy.sh
+++ copy.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Copy Neovim configuration files
-cp -r ~/.config/nvim/* /home/arthurvalls/configs/nvim/
+cp -r ~/.config/nvim/* /home/arthur/configs/nvim/
 
 # Copy Alacritty configuration files
-cp -r ~/.config/alacritty/* /home/arthurvalls/configs/alacritty/
+cp -r ~/.config/alacritty/* /home/arthur/configs/alacritty/
 
 # Copy tmux configuration file to Alacritty directory
-cp -r ~/.tmux.conf /home/arthurvalls/configs/alacritty/.tmux.conf
+cp -r ~/.tmux.conf /home/arthur/configs/alacritty/.tmux.conf
 
 # Copy Zsh configuration file
-cp -r ~/.config/ezsh/ezshrc.zsh /home/arthurvalls/configs/zsh/
+cp -r ~/.config/ezsh/ezshrc.zsh /home/arthur/configs/zsh/
 
 # Copy kitty conf
-cp -r ~/.config/kitty/* /home/arthurvalls/configs/kitty
+cp -r ~/.config/kitty/* /home/arthur/configs/kitty
 
 # Copy fish
-cp -r ~/.config/fish/* /home/arthurvalls/configs/fish/
+cp -r ~/.config/fish/* /home/arthur/configs/fish/
 
-cp -r ~/.config/git/* /home/arthurvalls/configs/git/
+cp -r ~/.config/git/* /home/arthur/configs/git/
 
 
 echo "Configuration files have been copied successfully."
--- fish/config.fish
+++ fish/config.fish
@@ -19,9 +19,6 @@ alias myip 'wget -qO- https://wtfismyip.com/text'
 # Show all except . .. , sort by recent, / at the end of folders
 alias l 'ls -lAhrtF'
 
-# This alias for empty is better as a function in ~/.config/fish/functions/empty.fish
-# alias empty='empty_file() { cat /dev/null > "$1"; }; empty_file'
-
 # List directories only
 alias lsdd 'ls -d */'
 
@@ -33,15 +30,6 @@ alias lz 'lazygit'
 
 # Alias for eza
 alias ez 'eza'
-
-# Note: The 'empty' alias from your Zsh config is more complex and is better
-# implemented as a Fish function. For a truly simple config with *only* aliases,
-# I've commented it out here. If you want to include it, you should define it
-# as a function in ~/.config/fish/functions/empty.fish:
-
-# function empty
-#     cat /dev/null > "$argv[1]"
-# end
 
 alias ga='git add'
 alias gap='ga --patch'
